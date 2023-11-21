4aa08e81c777a6f941b6acf31ac78f6dff67ab38
Hoceine <hoceineelidrissi@gmail.com>
2023-11-21 09:42:48 +0100

    add : the arrow right and left listeners

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -129,6 +129,13 @@ document.body.onkeyup = function (e) {
         }
     }
 };
+document.body.addEventListener('keydown', function (e) {
+    if (e.key === "ArrowLeft") {
+        fetchSequentialAyah(false);
+    } else if (e.key === "ArrowRight") {
+        fetchSequentialAyah(true);
+    }
+});
 function setAyatAlkursi() {
     document.getElementById("text").textContent = "ٱللَّهُ لَآ إِلَـٰهَ إِلَّا هُوَ ٱلْحَىُّ ٱلْقَيُّومُ ۚ لَا تَأْخُذُهُۥ سِنَةٌۭ وَلَا نَوْمٌۭ ۚ لَّهُۥ مَا فِى ٱلسَّمَـٰوَٰتِ وَمَا فِى ٱلْأَرْضِ ۗ مَن ذَا ٱلَّذِى يَشْفَعُ عِندَهُۥٓ إِلَّا بِإِذْنِهِۦ ۚ يَعْلَمُ مَا بَيْنَ أَيْدِيهِمْ وَمَا خَلْفَهُمْ ۖ وَلَا يُحِيطُونَ بِشَىْءٍۢ مِّنْ عِلْمِهِۦٓ إِلَّا بِمَا شَآءَ ۚ وَسِعَ كُرْسِيُّهُ ٱلسَّمَـٰوَٰتِ وَٱلْأَرْضَ ۖ وَلَا يَـُٔودُهُۥ حِفْظُهُمَا ۚ وَهُوَ ٱلْعَلِىُّ ٱلْعَظِيمُ ٢٥٥";
     document.getElementById("surah-name").textContent = "سورة البقرة";
